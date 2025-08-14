338725a7bf54778ab75ccce53ac1a9a5d574316a
kebin <tenjok69@gmail.com>
2025-08-14 18:00:44 -0500

    Todo funcionando correctamente

--- SENA_APP/programas/urls.py
+++ SENA_APP/programas/urls.py
@@ -5,7 +5,8 @@ app_name = 'programas'
 
 urlpatterns = [
     path('programas/', views.ProgramaListView.as_view(), name='lista_programas'),
-    path('programas/<int:pk>/', views.ProgramaDetailView.as_view(), name='detalle_programa'),
+    path('programas/editar<int:pk>/', views.ProgramaDetailView.as_view(), name='editar_programa'),
+    path('programas/eliminar/<int:pk>/', views.ProgramaDetailView.as_view(), name='eliminar_programa'),
     #path('crear_programa/', views.ProgramaCreateView.as_view(), name='crear_programa'),
     path('crear_programa/', views.crear_programa, name='crear_programa'),
 ]
